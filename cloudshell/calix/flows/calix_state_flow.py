56946b8fb792ba4a3e19160605b2e6baa44fd907
Costya <costya.y@qualisystems.com>
2022-02-14 00:14:16 +0200

    THis commit includes:
    1. Partial Autoload
    2. Run commands
    3. Save/Restore configurations

---
+++ cloudshell/calix/flows/calix_state_flow.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+from cloudshell.shell.flows.state.basic_flow import StateFlow
+
+
+class CalixStateFlow(StateFlow):
+    pass
---
+++ cloudshell/calix/helpers/exceptions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+
+class CalixBaseException(Exception):
+    """Base Calix exception."""
+
+
+class CalixSNMPException(CalixBaseException):
+    """Calix enable/disable SNMP configuration exception."""
+
+
+class CalixSaveRestoreException(CalixBaseException):
+    """Calix save/restore configuration exception."""
+
+class CalixSaveRestoreStatusException(CalixSaveRestoreException):
+    """Calix save/restore configuration exception."""
+
+class CalixConnectivityException(CalixBaseException):
+    """Calix connectivity exception."""
+
+
+class CalixFirmwareException(CalixBaseException):
+    """Calix load firmware exception."""
